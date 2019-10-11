f3b35c4b59c4d76b844223f5adc78b3c906f23d4
Suvorov Daniil <severecloud@gmail.com>
2019-10-12 01:20:44 +0300

    feat: add time log

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const config = require("./config.json")
 const PixelBot = require("./PixelBot.js")
 const Store = require("./Store")
 
+console.logCopy = console.log.bind(console);
+
+console.log = function(data)
+{
+    const d = new Date()
+    const currentDate = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}]`
+    this.logCopy(currentDate, data)
+};
+
 async function init() {
     for (let link of config.wssLinks) {
         await new Promise((resolve) => setTimeout(resolve, 3000)).then((r)=> {
